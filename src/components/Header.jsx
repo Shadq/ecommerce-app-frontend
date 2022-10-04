28b516f61a32ca3ced7dde1a511323cc11cde779
Shadq <fmeda2104@gmail.com>
2022-10-04 17:21:55 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header__container">
+      <div className="logo__container">
+        <h1>ECOMMERCE</h1>
+        <img
+          src="https://images.prismic.io/bendingspoons-staging/23b2019d-a840-4d29-bd18-c3a081d3a9be_Homepage_00000.png?auto=compress%2Cformat&rect=420%2C0%2C1080%2C1080&w=1024&h=1024&fm=webp"
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
